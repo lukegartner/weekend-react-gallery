dd42b05d8254aa6012d057d5ada295075848200d
Luke Gartner <lukegartner4@gmail.com>
2023-06-27 08:55:36 -0500

    add data to server

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,37 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 1,
+    path: "images/goat_small.jpg",
+    description: "Photo of a goat taken at Glacier National Park.",
+    likes: 0,
+  },
+  { id: 2, path: "images/1.jpg", description: "Girl Skateboarder", likes: 0 },
+  { id: 3, path: "images/2.jpg", description: "Bowl Sesh", likes: 0 },
+  { id: 4, path: "images/3.jpg", description: "Bowl at Sunset", likes: 0 },
+  {
+    id: 5,
+    path: "images/4.jpg",
+    description: "Hardflip in a sun hat",
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: "images/5.jpg",
+    description: "Frontside Boardslide a traffic barrier",
+    likes: 0,
+  },
+  {
+    id: 7,
+    path: "images/6.jpg",
+    description: "Tre Flip the Hip",
+    likes: 0,
+  },
+  {
+    id: 8,
+    path: "images/7.jpg",
+    description: "Big Boneless",
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
